03a022ff9c1a343068ddaac7b8b7da28a050238a
Jalalak <s6030213012@phuket.psu.ac.th>
2019-05-10 23:41:42 +0700

    install and edit login
    npm i jquery and edit login

--- login.js
+++ login.js
@@ -1,10 +1,70 @@
+$.validator.setDefaults({
+    submitHandler: function() {}
+});
+$.validator.methods.equal = function(value, element, param) {
+    return value == param;
+};
+$().ready(function() {
+    var validator = $("#form").bind("invalid-form.validate", function() {
+        $("#summary").html("Your form contains " + validator.numberOfInvalids() + " errors, see details below.");
+    }).validate({
+        debug: true,
+        errorElement: "em",
+        errorContainer: $("#warning, #summary"),
+        errorPlacement: function(error, element) {
+            error.appendTo(element.parent("div").next("span"));
+            $("#err").show();
+        },
+        success: function(label) {
+            label.text("").addClass("success");
+            $("#err").hide();
+        },
+        rules: {
+            email: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            username: {
+                required: "Please enter a email",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+});
 function authen() {
     var email = document.getElementById('#email');
     var password = document.getElementById('#password');
-    if (email == password) {
-        console.log('true')
-
+    if (!email ==  "admin@nw.com" && !password=="12345**") {
+        console.log('true');
+        console.log("Success");
+            setTimeout(window.location.href = "dashboard.html?");
     } else {
-        console.log('false')
+        console.log('false');
+        $("#err").show();
     }
 }
