ecd4397a19e76331d8261d6a046846e39abf01f0
Julalak Tammaphet <s6030213020@phuket.psu.ac.th>
2019-05-10 15:59:18 +0700

    add

---
+++ login.js
@@ -0,0 +1,10 @@
+function authen() {
+    var email = document.getElementById('#email');
+    var password = document.getElementById('#password');
+    if (email == password) {
+        console.log('true')
+
+    } else {
+        console.log('false')
+    }
+}
