4095a70b5bbee8e4992517980296c4a8fc0d50a3
Julalak Tammaphet <s6030213020@phuket.psu.ac.th>
2019-05-11 14:32:59 +0700

    edit

--- customer.js
+++ customer.js
@@ -1,12 +1,20 @@
 $(function() {
-   
-       
-        $.get("data/customers.json",function (){
-            console.log(data);
-            
 
-        });
-      
-        document.getElementById("show").innerHTML = r;
 
-});   
+            $.get("data/customers.json", function() {
+                    console.log(data);
+                    $('#datalist').append(tablehead);
+                    for (var index in movies) {
+                        var customer = movies[index];
+                        var tableRow = '<tr><td>' + customer.CustomerID + '</td><td>' + customer.CompanyName + '</td><td>' + customer.ContactName + '</td><td>' + customer.ContactTitle + '</td></tr>'
+                        $('#datalist').append(tableRow)
+                        console.log(customer);
+
+
+
+
+                    });
+
+                document.getElementById("show").innerHTML = r;
+
+            });
