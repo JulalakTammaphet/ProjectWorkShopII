60263441c7abf229da3d74fc9c57c00889373aaf
julalak <s6030213020@phuket.psu.ac.th>
2019-05-12 01:47:11 +0700

    edit cusdetail.js ver 1.3

---
+++ custdetail.js
@@ -0,0 +1,36 @@
+var urlSearchParams = URL.searchParams;
+let params = (new URL(document.location)).searchParams;
+let tid = params.get("id");
+let options = {};
+var show='';
+console.log(tid);
+
+$(document).ready(function() {
+    var url = "http://localhost:3000/customers?";
+    $.get(url, function(data) {
+        // console.log(data);          
+        var all =data.length;
+        var r ="";
+        for(var i =0;i<all;i++){
+            if(data[i].customerID===tid){
+            r += '<tr><td>'+data[i].customerID+'</td><td>'
+         +data[i].companyName+'</td><td>'+data[i].contactName+'</td><td>'+data[i].contactTitle+'</td><td>'
+         +"street :"+data[i].address.street+"<br>" +
+         "city :"+ data[i].address.city + "<br>" +
+         "region :"+ data[i].address.region + "<br>" +
+         "postalCode :"+ data[i].address.postalCode + "<br>" +
+         "country :"+ data[i].address.country + "<br>" +
+         "phone :"+ data[i].address.phone +
+         '</td></tr>';                   
+        }  
+    }
+        document.getElementById('dataTable').innerHTML = r;
+
+       show +="<div class='"+'bg-white py-2 collapse-inner rounded'+"'>"
+             + "<h6 class='"+'collapse-header'+"'>"+"Custom Components:</h6>"
+                   + "<a class='"+'collapse-item'+"'"+"href='"+'editcust.html?id='+tid+'"'+'>Edit Customer</a>'+
+                    +  "<a class='"+'collapse-item'+"'"+"href='"+'custdetail.html"'+'>Detail Customer</a>'+
+                    "</div>";
+                    ocument.getElementById('collapseTwo').innerHTML = show;          
+    });
+});
