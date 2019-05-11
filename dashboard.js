a084d699d05083508f7a97370ea77f1098132ade
julalak <s6030213020@phuket.psu.ac.th>
2019-05-11 18:47:43 +0700

    dashboard

--- dashboard.js
+++ dashboard.js
@@ -9,3 +9,40 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    var url = "http://localhost:3000/orders?";
+    $.get(url, function(data) {
+        console.log(data);
+        
+        var allOrder = data.length;
+        console.log(allOrder);
+        
+       var Order = allOrder
+        $('#totalOrder').append(Order);
+    });
+});
+
+$(document).ready(function() {
+    var url = "http://localhost:3000/suppliers?";
+    $.get(url, function(data) {
+        var allSupplier = data.length;
+        console.log(allSupplier);
+        
+       var Supplier = allSupplier;
+        $('#totalSupplier').append(Supplier);
+    });
+});
+
+$(document).ready(function() {
+    var url = "http://localhost:3000/customers?";
+    $.get(url, function(data) {
+        console.log(data);
+        
+        var allCustomer = data.length;
+        console.log(allCustomer);
+        
+       var Customer = allCustomer+("People");
+        $('#totalCustomer').append(Customer);
+    });
+});
+
