db8f1d51bb3010fd589f9223f83961505481147e
Julalak Tammaphet <s6030213020@phuket.psu.ac.th>
2019-05-11 16:46:03 +0700

    comment

--- customer.js
+++ customer.js
@@ -1,22 +1,22 @@
-$(function() {
+// $(function() {
 
 
-            $.get("data/customers.json", function() {
-                    console.log(data);
+//          $.get("data/customers.json", function() {
+//                  console.log(data);
 
-                    var customers = Search.Search;
-                    var tablehead = '<tr><th>CustomerID</th><th>CompanyName</th><th>ContactName</th><th>ContactTitle</th></tr>'
-                    $('#datalist').append(tablehead);
-                    for (var index in customers) {
-                        var customer = customers[index];
-                        var tableRow = '<tr><td>' + customer.CustomerID + '</td><td>' + customer.CompanyName + '</td><td>' + customer.ContactName + '</td><td>' + customer.ContactTitle + '</td></tr>'
-                       
+//                 var customers = Search.Search;
+//                 var tablehead = '<tr><th>CustomerID</th><th>CompanyName</th><th>ContactName</th><th>ContactTitle</th></tr>'
+//                 $('#datalist').append(tablehead);
+//                  for (var index in customers) {
+//                       var customer = customers[index];
+//                       var tableRow = '<tr><td>' + customer.CustomerID + '</td><td>' + customer.CompanyName + '</td><td>' + customer.ContactName + '</td><td>' + customer.ContactTitle + '</td></tr>'
 
-                    }
 
-                    document.getElementById("show").innerHTML = tableRow;
-                    });
+}
 
-                
+//                   document.getElementById("show").innerHTML = tableRow;
+//                   });
 
-            });
+
+
+//           });
