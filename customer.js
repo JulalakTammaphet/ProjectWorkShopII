9e43f27f4492f33a03c7fdb0199a5f8d4e566315
julalak <s6030213020@phuket.psu.ac.th>
2019-05-11 14:22:33 +0700

    customers
    edit customers v1.2

---
+++ customer.js
@@ -0,0 +1,12 @@
+$(function() {
+   
+       
+        $.get("data/customers.json",function (){
+            console.log(data);
+            
+
+        });
+      
+        document.getElementById("show").innerHTML = r;
+
+});   
