7719420c35dae38f353ce043befc255eae49f632
julalak <s6030213020@phuket.psu.ac.th>
2019-05-12 00:17:06 +0700

    edit url send value

--- customer.js
+++ customer.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var all =data.length;
         var r ="";
         for(var i =0;i<all;i++){
-            r += '<tr><td><a href="custdetail.html?id=?" '+data[i]["customerID"]+'>'+data[i].customerID+'</a></td><td>'
+            r += '<tr><td><a href='+'"custdetail.html?id='+data[i].customerID+'"'+">"+data[i].customerID+'</a></td><td>'
          +data[i].companyName+'</td><td>'+data[i].contactName+'</td><td>'+data[i].contactTitle+'</td></tr>';                   
         }  
         document.getElementById('dataTable').innerHTML = r;
