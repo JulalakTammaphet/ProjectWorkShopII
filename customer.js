59e4df791363b2e3f6e3a217139aeb8f1e132d19
julalak <s6030213020@phuket.psu.ac.th>
2019-05-11 23:27:59 +0700

    edit

--- customer.js
+++ customer.js
@@ -3,32 +3,26 @@ $(document).ready(function() {
     $.get(url, function(data) {
         // console.log(data);
            
-        var all =data.length;
-        var r ="";
-        for(var i =0;i<all;i++){
-             r += "<tr><td><a href="+'"custdetail.html?id='+data[i]["customerID"]+">"+i+"."+data[i]["customerID"]+"</a></td>"
-             +"<td>"+ data[i]["companyName"]+ "</td><td>"+
-             data[i]["contactName"]+"</td><td> "
-             +data[i]["contactTitle"]+"</td></tr>";
-
+        // var all =data.length;
+        // var r ="";
+        // for(var i =0;i<all;i++){
+        //      r += "<tr><td><a href="+'"custdetail.html?id='+data[i]["customerID"]+">"+i+"."+data[i]["customerID"]+"</a></td>"
+        //      +"<td>"+ data[i]["companyName"]+ "</td><td>"+
+        //      data[i]["contactName"]+"</td><td> "
+        //      +data[i]["contactTitle"]+"</td></tr>";         
              
-             
-             
-        }  
-        
-
-        
-        
-        // var customerData = '';
-        // $.each(data, function(key, value) {
-        //     customerData += '<tr>';
-        //     customerData += '<td>' + value.customerID + '</td>';
-        //     customerData += '<td>' + value.companyName + '</td>';
-        //     customerData += '<td>' + value.contactName + '</td>';
-        //     customerData += '<td>' + value.contactTitle + '</td>';
-        //     customerData += '</tr>';
-        // });
-        document.getElementById('dataTable').innerHTML = r;
+        // }  
+           
+        var customerData = '';
+        $.each(data, function(key, value) {
+            customerData += '<tr>';
+            customerData += '<td><a href='+'"editcust.html?id="'+ value.customerID+ ">" + value.customerID + '</a></td>';
+            customerData += '<td>' + value.companyName + '</td>';
+            customerData += '<td>' + value.contactName + '</td>';
+            customerData += '<td>' + value.contactTitle + '</td>';
+            customerData += '</tr>';
+        });
+        document.getElementById('dataTable').innerHTML = customerData;
     });
 });
 
