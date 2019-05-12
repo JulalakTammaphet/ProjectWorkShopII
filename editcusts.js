b9b09c365f857e5075c87b4d6ddd9483cc7a986f
julalak <s6030213020@phuket.psu.ac.th>
2019-05-12 16:30:59 +0700

    edit

--- editcusts.js
+++ editcusts.js
@@ -24,7 +24,6 @@ $(document).ready(function() {
                     '</td></tr>';
                     // var ids = data[i].customerID;
 
-                    $("#customerID").val(data[i].customerID);  
                     $("#companyName").val(data[i].companyName); 
                     $("#contactName").val(data[i].contactName);
                     $("#contactTitle").val(data[i].contactTitle);
@@ -33,13 +32,41 @@ $(document).ready(function() {
                     $("#region").val(data[i].address.region);     
                     $("#postalCode").val(data[i].address.postalCode); 
                     $("#country").val(data[i].address.country);  
-                    $("#phone").val(data[i].address.phone);    
-            }
+                    $("#phone").val(data[i].address.phone); 
+                    
+                    $("#save").click(function () {
+                        var newuser = {};
+                       // newuser.id = data.id;
+                        newuser[i].customerID = $("#customerID").val();
+                        newuser[i].contactName = $("#contactName").val();
+                        newuser[i].contactTitle = $("#contactTitle").val();
+                        newuser[i].address.street = $("#street").val();
+                        newuser[i].address.city = $("#city").val();
+                        newuser[i].address.region = $("#region").val();
+                        newuser[i].address.postalCode = $("#postalCode").val();
+                        newuser[i].address.country = $("#country").val();
+                        newuser[i].address.phone = $("#phone").val();
+                        console.log(JSON.stringify(newuser));
+                        var updateUrl = "http://localhost:3000/customers/" + data[i].customerID ;
+                        $.ajax({
+                            url: updateUrl,
+                            type: 'PUT',
+                            data: newuser,
+                            success: function (result) {
+                                console.log('Updated!');
+                            }
+                        });
+                        $("#err").show();
+                        setTimeout(location.reload.bind(location), 900);
+                    });
+                }
+            
+                }
             
             
-        }
+        });
         
     });
-});
+
 
 
